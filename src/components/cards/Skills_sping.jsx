acc24ba1122540851f9a8af881053d2fd078c5c2
Rohit kumar <rohitkumar9643017@gmail.com>
2025-06-12 22:42:35 +0530

    update

--- src/components/cards/Skills_sping.jsx
+++ src/components/cards/Skills_sping.jsx
@@ -7,7 +7,7 @@ const LoadingCircle = ({icon: Icon}) => {
           AOS.init();
         }, []);
   return (
-    <div className=" w-full flex justify-center group" data-aos="fade-up">
+    <div className=" w-full flex justify-center group" >
       <div className="relative h-[6rem] md:h-[8rem] w-[6rem] md:w-[8rem]  rotate-45 border-[7px] border-white group-hover:border-[#ff0000] animate-[move_0.5s_linear_infinite_alternate-reverse] flex items-center justify-center group-hover:animate-none  ">
         <div className="h-[4.7rem] md:h-[6.7rem] w-[4.7rem] md:w-[6.7rem]  border-x-[#ff0000] border-y-black border-double border-transparent group-hover:border-x-[#000000] border-[7px]  group-hover:animate-none flex items-center justify-center">
           <Icon className={`text-4xl md:text-[4rem] group-hover:text-[#ff0000] text-black  rotate-[-45deg]`} />
